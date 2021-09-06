63483d24638424883aa9a34bfa7678046b6a075f
hanadot <hanahanica@icloud.com>
2021-09-06 11:20:55 +0000

    maandag 6 sept

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+var x = 50;
+var y = 50;
+var speedX = 2;
+var speedY = 1;
+
 /**
  * setup
  * de code in deze functie wordt één keer uitgevoerd door
@@ -18,9 +23,20 @@ function setup() {
  * uitgevoerd door de p5 library, nadat de setup functie klaar is
  */
 function draw() {
+  background('blue');
   // stel vulkleur in
   fill(100, 100, 255);
 
   // teken een cirkel
-  ellipse(50,50,80,80);
+  ellipse(x,y,80,80);
+  
+//positie
+  x = x + speedX;
+  y = y + speedY;
+
+  if (y === 720){
+    speedY= speedY - 1;
+
+
+  }
 }
